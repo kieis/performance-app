329d9d477c7e93e21cde50ab6a56c586dcfad59a
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2023-04-30 20:48:12 -0300

    chore: added memo

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,3 +1,12 @@
+import { memo } from "react";
+
+/* memo usage situations
+ * 1. Pure Functional Components
+ * 2. Renders too often
+ * 3. Re-renders with same props
+ * 4. Medium to big size components, to see some real effect
+ */
+
 interface ProductItemProps {
   product: {
     id: number;
@@ -6,10 +15,17 @@ interface ProductItemProps {
   };
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product }: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
     </div>
   );
 }
+
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product); //check if properties are equals
+  }
+);
